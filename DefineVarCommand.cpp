546269f33546d2d1c729bb237732cecefde64058
shiraz berger <shiraz422@gmail.com>
2019-12-17 14:32:44 +0200

    update 8 - nili1

--- DefineVarCommand.cpp
+++ DefineVarCommand.cpp
@@ -10,6 +10,7 @@ class DefineVarCommand : public Command {
  public:
   Singleton *singleton = Singleton::getSingleton();
   virtual void execute(queue<string> &token) {
+    bool is_digit = false;
     //the word var
     token.pop();
     string var_name = token.front();
@@ -23,9 +24,26 @@ class DefineVarCommand : public Command {
       //pop the var or value after the =
       string other_var_name = token.front();
       token.pop();
-      //if after the = is a var:
-      //insert var_name and the var that comes after the = to commands
-      singleton->symbol_table_program.insert({var_name, singleton->symbol_table_program.at(other_var_name)});
+      float num;
+      try {
+        singleton->symbol_table_program.at(other_var_name);
+        //this is a number or expression
+      } catch (exception e){
+        num = atof(other_var_name.c_str());
+        is_digit = true;
+      }
+      //if after the = is a number or expression
+      if (is_digit) {
+        Var *v = new Var("", "");
+        v->setValue(num);
+        singleton->symbol_table_program.insert({var_name, v});
+
+      } else {
+        //if after the = is a var:
+        //insert var_name and the var that comes after the = to commands
+        singleton->symbol_table_program.insert({var_name, singleton->symbol_table_program.at(other_var_name)});
+      }
+
       //the action is direction
     } else {
       //direction
